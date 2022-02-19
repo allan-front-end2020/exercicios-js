640f783a87a5a03b2baee7b24dfc516c7cb6d8a8
Allan Tavares <allan.tavares@estudante.ufla.br>
2022-02-19 11:32:00 -0300

    tipos de variaveis

--- js.js
+++ js.js
@@ -1,2 +1,14 @@
+ //  Variaveis tipo strings 
+ var text = 'Curso de Js'
 
-alert(' olá seja bem vindo ao curso')
+ /// variaves  tipo number
+
+ var numeroInteiro = -5
+
+ var numeroFracionario = 2.00
+
+
+ //// variaves tipo booleanas 
+
+ var teste = 'true'
+ var teste = 'false'
